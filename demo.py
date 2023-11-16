388154b57b06bb452e0922d10dfd2fdc4070a1b8
amaneth <amankassahun190@gmail.com>
2023-11-16 10:48:33 +0300

    added the detailing analysis work

--- demo.py
+++ demo.py
@@ -8,9 +8,9 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.vectorstores.base import Document
+from langchain.schema.document import Document
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain,HypotheticalDocumentEmbedder
 from tqdm import tqdm
 import tiktoken
 from io import StringIO
@@ -37,10 +37,11 @@ embed = OpenAIEmbeddings(
 )
 
 
-model_name = "gpt-4-32k"
-llm = OpenAI(model_name=model_name)
+analysing_model = OpenAI(model_name="gpt-4")
 
-qa_chain = load_qa_chain(llm, chain_type="stuff")
+extracting_model = OpenAI(model_name="gpt-4-1106-preview")
+
+qa_chain = load_qa_chain(analysing_model, chain_type="stuff")
 
 pinecone.init(
     api_key=PINECONE_API_KEY,
@@ -52,26 +53,10 @@ index_name = "lta-test"
 
 
 task_info = """You are a customer service for an app that give funding predictions for various agencies,insights,
-analysis and trends for a comapny in interested in health related RFPs based on the documents provided.
-The documents include information on government agency budgets,
-grant allocations, and financial reports. The user may ask related or non-related questions, answer to the following user question accordingly:  """
-
-sample_response = '''The documents highlight multiple entities receiving allocations, with an explicit emphasis on health-related programs and initiatives. 
-
-1. Department of Health and Human Services (HHS): The HHS received significant amounts for a variety of health-focused activities. In 2010, it was authorized to receive $100,000,000 for infrastructure expansion on healthcare facilities for research, inpatient tertiary care, and outpatient clinical services at academic health centers. Moreover, significant funds have been marked for community health centers and the National Health Service Corps Fund (CHC Fund).
-
-2. National Health Service Corps: This entity, within the HHS, received a significant escalating allocation each fiscal year starting from $320,461,632 in 2010, gradually increasing to $1,154,510,336 by 2015. 
-
-3. Prevention and Public Health Fund: Allocated an escalating amount starting from $500,000,000 in 2010, which increases to $2,000,000,000 in 2015 and each fiscal year thereafter.
-
-4. Community Health Center Fund (CHC Fund): This fund mainly focuses on expanded and sustained national investment in community health centers and the National Health Service Corps, with funds increasing from $1,000,000,000 in 2011 to $3,600,000,000 in 2015.
-
-Considering these allocations, the Department of Health and Human Services, the National Health Service Corps, and various health and prevention funds are likely recipients of funding. 
-
-In terms of Requests for Proposals (RFPs), one could anticipate focus areas including debt service on, or the direct construction or renovation of healthcare facilities, public health and prevention programs, community health centers, and health services corps. There might be special emphasis on providing greater access to health care within States, and supporting the financial viability of the State’s public medical and dental school as well as its academic health centers.
-
-Favorable project categories for funding would likely include health infrastructure construction and renovation, health services program expansion, community preventive health activities aimed at reducing chronic disease rates and health disparities, and professional development in the health sector. Projects that address social determinants of health, disparities in health access and outcomes, and those focusing on prevention would also be prioritized.
-'''
+analysis and trend. The documents include information on government agency budgets,
+grant allocations, and financial reports. Provide a breakdown and as detail answer as possible with relevant 
+budget numbers for all agencies. You can make predictions where the budgets will go based on the information provided.
+The user may ask related or non-related questions, answer to the following user question accordingly:  """
 
 
 
@@ -88,7 +73,7 @@ vectorstore = Pinecone(
 tiktoken.encoding_for_model('text-embedding-ada-002')
 
 # Intialize a tiktoken tokenizer (i.e. a tool that identifies individual tokens (words))
-tokenizer = tiktoken.get_encoding('cl100k_base')
+#tokenizer = tiktoken.get_encoding('cl100k_base')
 
 def tiktoken_len(text: str) -> int:
     """
@@ -113,7 +98,6 @@ def extract_text_from_pdf(pdf_file_path):
 
 
 def chunk_by_section(document):
-    token_limit = 6000
     text = extract_text_from_pdf(document)
     # Define a regular expression pattern to match section headers
     section_pattern = re.compile(r'SEC\.')
@@ -123,7 +107,7 @@ def chunk_by_section(document):
     # Remove empty strings from the list
     sections = [section.strip() for section in sections if section.strip()][1:]
     print(len(sections))
-    selected_sections = [section for section in sections if tiktoken_len(section) < token_limit]
+    selected_sections = [section for section in sections]
     
     return selected_sections
 
@@ -143,17 +127,26 @@ def chunk_by_size(text: str, size: int = 200) -> list[Document]:
 )
     return text_splitter.create_documents([text])
 
-def divide_into_groups(documents, k):
-    # Calculate the total number of groups needed
-    num_groups = len(documents) // k + (len(documents) % k > 0)
+def divide_into_groups(chunks):
+    groups = []
+    start_index = 0
+    group_size = 2  # Initial group size
 
-    # Create empty groups
-    groups = [[] for _ in range(num_groups)]
-
-    # Distribute documents into groups
-    for i, doc in enumerate(documents):
-        group_index = i % num_groups
-        groups[group_index].append(doc)
+    while start_index < len(chunks):
+        group = chunks[start_index:start_index + group_size]
+        groups.append(group)
+        start_index += group_size
+        group_size *= 2  # Double the group size for the next iteration
+    
+    last_group = len(groups[-1]) 
+    before_last = len(groups[-2])
+    if last_group < (before_last//2):
+        groups[-2] = groups[-1]+groups[-2]
+        groups = groups[:-1]
+    elif last_group < (before_last):
+        temp = groups[-1]
+        groups[-1] = groups[-2]
+        groups[-2] = temp
 
     return groups
 
@@ -165,16 +158,16 @@ def merge_with_numbering(texts):
 
     return merged_text
 
-def chunk_summary(docs, namespace):
+def chunk_summary(docs, namespace, depth):
 
     print('funding summary info extraction in progress...')
     summary_prompt = PromptTemplate(
         input_variables = ['chunk'],
-        template = "Summarize the funding related information from the following document. The summary will be used to funding analysis and predictions. Document: {chunk}"
+        template = "Extract the funding related information from the following documents. The information will be used to funding analysis and predictions. Documents: {chunk}"
     )
 
-    chunks = divide_into_groups(docs, k=3)
-    chain = LLMChain(llm=llm, prompt=summary_prompt)
+    chunks = divide_into_groups(docs)
+    chain = LLMChain(llm=extracting_model, prompt=summary_prompt)
 
     funding_summaries = []
     for chunk in tqdm(chunks):
@@ -182,9 +175,10 @@ def chunk_summary(docs, namespace):
         funding_summaries.append(summary)
 
     summary = merge_with_numbering(funding_summaries)
-
-    with open('summary/'+namespace+'.smry', 'w') as file:
-        file.write(summary)
+    
+    if depth=='Extensive':
+        with open('summary/'+namespace+'.smry', 'w') as file:
+            file.write(summary)
 
 
     return summary
@@ -207,15 +201,20 @@ def get_answer(query, context):
 
 def get_funding_info(uploaded_file, file_path, namespace, query, depth):
 
-    depth_value = {'Light':3, 'Medium':15, 'Extensive':45}
+    depth_value = {'Light':14, 'Medium':30, 'Extensive':62}
     if depth=='Memory':
         with open(file_path, 'r') as file:
             funding_summary = file.read()
         logging.info('summary already in the disk, loaded summary:'+file_path+', size:'+str(len(funding_summary)))
     else:
+        prompt_template = """Give me funding predictions, Which agencies will be funded.
+        Question: {question}
+        Answer:"""
+
+        prompt = PromptTemplate(input_variables=["question"], template=prompt_template)
+        llm_chain = LLMChain(llm=extracting_model, prompt=prompt)
+        hyde_embed = HypotheticalDocumentEmbedder(llm_chain=llm_chain, base_embeddings=embed)
         index = pinecone.Index(index_name)
-        with open('hyde_embedding.pkl', 'rb') as f:
-            hyde_embed = pickle.load(f)
 
         if namespace not in index.describe_index_stats()['namespaces']:
             pdf_file = io.BytesIO(uploaded_file.getvalue())
@@ -231,12 +230,12 @@ def get_funding_info(uploaded_file, file_path, namespace, query, depth):
             vectorstore = Pinecone.from_texts(sections, hyde_embed, index_name=index_name, namespace=namespace)
 
         else:
-            vectorstore = Pinecone(index, hyde_embed.embed_query, text_field)
+            vectorstore = Pinecone(index, embed.embed_query, text_field)
 
         #similar_query = 'Give me a funding prediction and analysis for various health related agencies' 
         print('Doing analysis with:'+depth)
         similar_docs = get_similiar_docs(vectorstore, query, namespace, k=depth_value[depth])
-        funding_summary = chunk_summary(similar_docs, namespace)
+        funding_summary = chunk_summary(similar_docs, namespace, depth)
 
     return funding_summary
 
